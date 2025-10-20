1ea8dc7979d74ea10a0cc2c08c20057870b0944b
nimscore <nimscore@gmail.com>
2025-10-20 18:03:43 +0300

    feat: gRPC-getaway, get communities by slug/id, remove gql, swagger doc, getaway server

--- service/community/internal/orm/community.go
+++ service/community/internal/orm/community.go
@@ -31,3 +31,83 @@ func (this *PostgresClient) InsertCommunity(community *Community) error {
 	transaction := this.database.Create(community)
 	return transaction.Error
 }
+
+func (this *PostgresClient) SelectCommunityByID(id string) (*Community, error) {
+	var community Community
+	transaction := this.database.
+		Select([]string{
+			"id",
+			"owner_id",
+			"name",
+			"description",
+			"slug",
+			"created_at",
+			"updated_at",
+		}).
+		Where("id = ?", id).
+		First(&community)
+
+	if transaction.Error != nil {
+		return nil, transaction.Error
+	}
+
+	return &community, nil
+}
+
+func (this *PostgresClient) SelectCommunityBySlug(slug string) (*Community, error) {
+	var community Community
+	transaction := this.database.
+		Select([]string{
+			"id",
+			"owner_id",
+			"name",
+			"description",
+			"slug",
+			"created_at",
+			"updated_at",
+		}).
+		Where("slug = ?", slug).
+		First(&community)
+
+	if transaction.Error != nil {
+		return nil, transaction.Error
+	}
+
+	return &community, nil
+}
+
+func (this *PostgresClient) SelectCommunitiesWithPagination(limit int, cursor string) ([]*Community, error) {
+	var communities []*Community
+
+	query := this.database.
+		Select([]string{
+			"id",
+			"owner_id",
+			"name",
+			"description",
+			"slug",
+			"created_at",
+			"updated_at",
+		}).
+		Order("created_at DESC, id DESC")
+
+	// Если передан cursor, используем его для пагинации
+	if cursor != "" {
+		var cursorCommunity Community
+		if err := this.database.Where("id = ?", cursor).First(&cursorCommunity).Error; err == nil {
+			// Продолжаем с элементов, которые созданы раньше или имеют меньший ID
+			query = query.Where("(created_at < ?) OR (created_at = ? AND id < ?)",
+				cursorCommunity.CreatedAt,
+				cursorCommunity.CreatedAt,
+				cursorCommunity.ID)
+		}
+	}
+
+	transaction := query.Limit(limit).Find(&communities)
+
+	if transaction.Error != nil {
+		return nil, transaction.Error
+	}
+
+	return communities, nil
+}
